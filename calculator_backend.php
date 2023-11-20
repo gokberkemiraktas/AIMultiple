5d62f398c1907a0ec2910a087cc4a68e8983f494
gokberkemiraktas <74544669+gokberkemiraktas@users.noreply.github.com>
2023-11-20 23:00:49 +0300

    done

--- calculator_backend.php
+++ calculator_backend.php
@@ -1,49 +1,69 @@
 <?php
 header("Access-Control-Allow-Methods: GET");
-try {
-    $calculations = [];
-    $num1 = htmlspecialchars($_GET["num1"]);//security for xss
-    $num2 = htmlspecialchars($_GET["num2"]);//security for xss
-    $operator = htmlspecialchars($_GET["operator"]);//security for xss
-    if (!isset($num1) || !isset($num2) || !isset($operator)) {
-        throw new Exception('Missing parameters');
+$action = htmlspecialchars($_GET['action']);//security for xss
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($action)) {
+    if ($action == 'calculator')
+        calculator();
+    elseif ($action == 'bot')
+        bot();
+    else
+        echo "Invalid action";
+}
+function calculator()
+{
+    try {
+        $num1 = htmlspecialchars($_GET["num1"]);//security for xss
+        $num2 = htmlspecialchars($_GET["num2"]);//security for xss
+        $operator = htmlspecialchars($_GET["operator"]);//security for xss
+        if (!isset($num1) || !isset($num2) || !isset($operator)) {
+            throw new Exception('Missing parameters');
+        }
+        if (!is_numeric($num1) || !is_numeric($num2) || !(1 <= $operator && $operator <= 4)) {
+            throw new Exception('Invalid parameters');
+        }
+        echo performCalculation($num1, $operator, $num2);
+    } catch (Exception $e) {
+        echo 'Caught exception: ', $e->getMessage(), "\n";
     }
-    if (!is_numeric($num1) || !is_numeric($num2) || !(1 <= $operator && $operator <= 4)) {
-        throw new Exception('Invalid parameters');
-    }
-
-    echo performCalculation($num1, $operator, $num2);
-    for ($i = 0; $i < 1000; $i++) {
-        $num1 = rand(1, 10);
-        $num2 = rand(1, 10);
-        $operator = rand(1, 4);
-
-        $startTime = microtime(true);
-        $result = performCalculation($num1, $operator, $num2);
-        $endTime = microtime(true);
-        $duration = $endTime - $startTime;
-
-        $calculations[] = [
-            'num1' => $num1,
-            'operator' => $operator,
-            'num2' => $num2,
-            'result' => $result,
-            'duration' => $duration,
-        ];
-    }
-
-    $jsonData = json_encode($calculations, JSON_PRETTY_PRINT);
-    $directoryName = "calculations";
-    if (!is_dir($directoryName)) {
-        mkdir($directoryName, 0777, true);
-    }
-    $fileName = $directoryName . '/' . date('Y-m-d-H-i-s') . '.json';
-    file_put_contents($fileName, $jsonData);
-
-} catch (Exception $e) {
-    echo 'Caught exception: ', $e->getMessage(), "\n";
 }
 
+function bot()
+{
+    try {
+        $calculations = [];
+        for ($i = 0; $i < 1000; $i++) {
+            $num1 = rand(1, 10);
+            $num2 = rand(1, 10);
+            $operator = rand(1, 4);
+
+            $startTime = microtime(true);
+            $result = performCalculation($num1, $operator, $num2);
+            $endTime = microtime(true);
+            $duration = $endTime - $startTime;
+
+            $calculations[] = [
+                'num1' => $num1,
+                'operator' => $operator,
+                'num2' => $num2,
+                'result' => $result,
+                'duration' => $duration,
+            ];
+        }
+
+        $jsonData = json_encode($calculations, JSON_PRETTY_PRINT);
+        $directoryName = "calculations";
+        if (!is_dir($directoryName)) {
+            mkdir($directoryName, 0777, true);
+        }
+        $fileName = $directoryName . '/' . date('Y-m-d-H-i-s') . '.json';
+        file_put_contents($fileName, $jsonData);
+        echo "File saved to " . $fileName . " successfully".PHP_EOL;
+        echo "<a href='$fileName' target='_blank'>Open file</a>";
+
+    } catch (Exception $e) {
+        echo 'Caught exception: ', $e->getMessage(), "\n";
+    }
+}
 
 function performCalculation($num1, $operator, $num2)
 {
